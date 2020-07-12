93d4cd148cec6319ef95d0e35639c8a80efc830b
elbywan <elbywan@hotmail.com>
2020-07-12 13:08:32 +0200

    timestamp: include comparable

--- src/bson/timestamp.cr
+++ src/bson/timestamp.cr
@@ -1,9 +1,21 @@
 struct BSON
   # Timestamp - Special internal type used by MongoDB replication and sharding. First 4 bytes are an increment, second 4 are a timestamp.
   struct Timestamp
+    include Comparable(Timestamp)
+
     getter t, i
 
     def initialize(@t : UInt32, @i : UInt32)
+    end
+
+    def <=>(other : Timestamp)
+      timestamp_comparison = self.t <=> other.t
+
+      if timestamp_comparison == 0
+        self.i <=> other.i
+      else
+        timestamp_comparison
+      end
     end
 
     def to_json(builder : JSON::Builder)
