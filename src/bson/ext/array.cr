685f054d2d5d53d930f71d57bb3bb0047a0c0885
Holden Omans <holden.omans@gmail.com>
2021-04-19 09:19:27 -0400

    Update array.cr
    differentiate between hash and array when de-serializing an Array

--- src/bson/ext/array.cr
+++ src/bson/ext/array.cr
@@ -4,16 +4,38 @@ class Array(T)
     {% begin %}
     {% types = T.union_types %}
 
+    {% if types.select(&.<=(Hash)).size > 1 %}
+    {% raise "Unable to deserialize #{@type.id}. Can only have one Hash value type." %}
+    {% end %}
+
+    {% if types.select(&.<=(Array)).size > 1 %}
+    {% raise "Unable to deserialize #{@type.id}. Can only have one Array value type." %}
+    {% end %}
+
     arr = self.new
 
-    bson.each do |_, v|
-      case v
+    bson.each do |(_, v, code, _)|
+      case {v, code}
+      {% htyp = types.find(&.<=(Hash)) %}
+      {% if htyp %}
+      when {BSON, BSON::Element::Document}
+        arr << {{ htyp }}.from_bson(v)
+      {% end %}
+
+      {% atyp = types.find(&.<=(Array)) %}
+      {% if atyp %}
+      when {BSON, BSON::Element::Array}
+        arr << {{ atyp }}.from_bson(v)
+      {% end %}
+
       {% for typ in types %}
-        {% if typ <= BSON::Serializable || typ.class.has_method? :from_bson %}
-        when BSON
+        {% if typ <= Hash || typ <= Array %}
+
+        {% elsif typ <= BSON::Serializable || typ.class.has_method? :from_bson %}
+        when {BSON, _}
           arr << {{ typ }}.from_bson(v)
         {% else %}
-        when {{ typ }}
+        when { {{ typ }}, _}
           arr << v.as({{ typ }})
         {% end %}
       {% end %}
