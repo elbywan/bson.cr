b91ff7d810a47121855d787649196e9949ef524a
Anton Maminov <anton.linux@gmail.com>
2024-05-12 12:14:49 +0300

    fix nested annotations

--- src/bson/serializable.cr
+++ src/bson/serializable.cr
@@ -1,28 +1,4 @@
 module BSON::Serializable
-  # This annotation can be used to set global serialization options.
-  #
-  # ```
-  # # Use `camelize: "lower"` or `camelize: true` to set lower or higher camelcased properties on the target BSON.
-  # @[BSON::Options(camelize: true)]
-  # struct Model
-  #   include BSON::Serializable
-  #   # (…)
-  # end
-  # ```
-  annotation BSON::Options
-  end
-
-  # This annotation can be used to ignore or rename properties.
-  #
-  # ```
-  # @[BSON::Field(ignore: true)]
-  # property ignored_property : Type
-  # @[BSON::Field(key: new_name)]
-  # property renamed_property
-  # ```
-  annotation BSON::Field
-  end
-
   macro included
     {% verbatim do %}
 
